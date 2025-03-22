d5828626707c88b04cc48341464f8b0aa7430b50
pedro <pedrolucasgonza31@gmail.com>
2025-03-22 16:07:53 -0300

    up

---
+++ script.js
@@ -0,0 +1,8 @@
+// Exemplo de interação de curtida e comentário
+document.querySelector('.like-btn').addEventListener('click', function() {
+    alert('Você curtiu o post!');
+});
+
+document.querySelector('.comment-btn').addEventListener('click', function() {
+    alert('Comentando no post...');
+});
