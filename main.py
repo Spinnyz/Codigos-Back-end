6ee19f8a298e9e156bcae171e4e514f841693858
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-06-25 18:14:41 -0300

    up

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-saldo = 1000
-saque_dia = 500
-limite_saque = 200
+print("Bem-vindo ao Seara Bancus")
 
-x = saque_dia <= saldo and saque_dia >=limite_saque
-print (x)
-y =  limite_saque >= saque_dia or saque_dia <= saldo
-print (y)
+saldo = 1000
+
+while True:
+    escolha = input("\nO que você deseja fazer? \n 1 - Depositar \n 2 - Sacar \n 3 - Ver Saldo \n 4 - Sair \n")
+
+    if escolha == "1":
+        deposito = int(input("Quanto você deseja depositar? \n"))
+        saldo += deposito
+        print(f"Seu novo saldo é: {saldo}")
+
+    elif escolha == "2":
+        saque = int(input("Quanto você deseja sacar? \n"))
+        if saque > saldo:
+            print("Saldo insuficiente")
+        else:
+            saldo -= saque
+            print(f"Seu novo saldo é: {saldo}")
+
+    elif escolha == "3":
+        print(f"Seu saldo é: {saldo}")
+
+    elif escolha == "4":
+        print("Obrigado por usar o Seara Bancus, volte sempre!")
+
+    else:
+        print("Opção inválida. Tente novamente!")
