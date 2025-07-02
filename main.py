e2ac6488d2f2036b810a12f27093c96d053ddf01
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-07-02 17:25:50 -0300

    up

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 opções = (f"""
-[D]- Deposito
-[S]- Saque
-[E]- Extrato
-[Q]- Sair
+[D] - Depósito
+[S] - Saque
+[E] - Extrato
+[Q] - Sair
 """)
-
 
 saque = 0
 deposito = 0
@@ -13,35 +12,48 @@ saldo = 0
 extrato = ""
 LIMITE_SAQUE = 3
 
-
-
 while True:
-    print (opções)
+    print(opções)
     escolha = input(f"Digite a opção desejada:").upper().strip()
 
-    #Deposito
+    # Depósito
     if escolha == "D":
-        dep = float(input("Qual o valor de deposito:")) 
-        #Negativo
+        dep = float(input("Qual o valor do depósito:")) 
+        # Negativo
         if dep <= 0:
-             print("Valor invalido tente novamente")
-             continue
-    
-    #deposito correto
-        else: 
-            saldo+=dep
-            print(f" Seu novo saldoX é {saldo:.2f}")
-            #extrato
-            extrato += f"Deposito no valor de  R${dep}"
-
-    #saque
-    elif escolha == "S":
-        saq = float(input("Digite o valor de Saque:"))
-    #Saque sem o saldo
-        if saq > saldo:
-            print("Valor insificiente")
+            print("Valor inválido, tente novamente")
             continue
-    #Saque negativo
+
+        # Depósito correto
+        else: 
+            saldo += dep
+            print(f"Seu novo saldo é R$ {saldo:.2f}")
+            # Extrato
+            extrato += f"Depósito no valor de R$ {dep}\n"
+
+    # Saque
+    elif escolha == "S":
+        # Sem limite
+        if LIMITE_SAQUE == 0:
+            print("Você já fez as 3 transações do dia")
+            continue
+        else:
+            saq = float(input("Digite o valor do saque:"))
+
+        # Saque sem saldo
+        if saq > saldo:
+            print(f"Valor insuficiente para saque. Seu saldo atual é de R$ {saldo}")
+            continue
+        # Saque negativo
         elif saq <= 0:
-            print ("O valor minimo é R$1")
-        
+            print("O valor mínimo é R$ 1")
+            continue
+        # Sem saldo
+        elif saldo <= 0:
+            print("Sem saldo para saque")
+            continue
+        # Saque autorizado
+        else:
+            saldo -= saq
+            print(f"Seu novo saldo é R$ {saldo}")
+            LIMITE_SAQUE -= 1
