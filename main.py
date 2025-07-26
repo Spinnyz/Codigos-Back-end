1c96012050fab6d79ee0baeea35579cc08357342
Pedro Lucas <pedrolucasgonza31@gmail.com>
2025-07-26 12:54:35 -0300

    up

--- main.py
+++ main.py
@@ -21,3 +21,13 @@ def sacar(*, saldo, valor, extrato, limite=500, numero_saques=0, limite_saques=3
     return saldo, extrato, numero_saques
 
 #extrato
+def exibir_extrato(saldo,extrato):
+    print("\n===== EXTRATO =====")
+    if extrato:
+        print(extrato)
+    else:
+        print("Não foram realizadas movimentações.")
+    print(f"\nSaldo: R$ {saldo:.2f}")
+    print("====================")
+
+
